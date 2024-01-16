f83aa6e3775f51b9696cfaebf23a82724f069d03
yerti <yerti0818@gmail.com>
2024-01-16 14:24:30 -0500

    change first line of execution

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,8 +1,9 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 
 const oneFile = require('fs');
 
 const path = process.argv[2];
+
 oneFile.readFile(path, 'utf-8', (err, data) => {
     if (err) console.error(err);
     else console.log(data);
