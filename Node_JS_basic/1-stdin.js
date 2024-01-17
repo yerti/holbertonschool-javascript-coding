3022411c9c43ea70236f65448ce76249587a2dd9
yerti <yerti0818@gmail.com>
2024-01-17 11:51:46 -0500

    Fix: close command

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,31 @@
-console.log('Welcome to Holberton School, what is your name?');
+const readline = require('readline');
 
-process.stdin.on('data', (input) => {
-  process.stdout.write(`Your name is: ${input}`);
-  process.stdin.end();
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
-process.on('exit', () => {
+
+const isTTY = process.stdin.isTTY;
+
+if (isTTY) {
+  console.log('Welcome to Holberton School, what is your name?');
+}
+
+rl.on('line', (input) => {
+  console.log(`Your name is: ${input}`);
   console.log('This important software is now closing');
+  rl.close();
 });
+
+// Manejar la entrada no interactiva (a través de tubería)
+if (!isTTY) {
+  let input = '';
+  process.stdin.on('data', (chunk) => {
+    input += chunk;
+  });
+
+  process.stdin.on('end', () => {
+    rl.emit('line', input.trim());
+  });
+}
+
